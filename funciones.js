58e2239001531698d392dd2e64bd92abd7a85b9e
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-06-28 16:47:03 -0600

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,14 +1,14 @@
-/*forma 1
+//forma 1
 function saludo(){
     alert('hola');
 }
 saludo();
-*/ 
+ 
 //FORMA 2 ASIGANA UNA FUNCION A UNA VARIABLE
 const saludo= function(){
     console.log('Hola');
 };
-saludo();
+//saludo();
 
 //funciones de tipo flecha 
 const saludo2 = () => console.log('Hola desde flecha');
